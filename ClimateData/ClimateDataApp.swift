6e3d449840e7c7497965d7c8adbd596c6c5c041d
Bosco Ho <boscojwho@gmail.com>
2024-08-03 22:33:48 -0700

    Initial Commit

---
+++ ClimateData/ClimateDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimateDataApp.swift
+//  ClimateData
+//
+//  Created by Bosco Ho on 2024-08-03.
+//
+
+import SwiftUI
+
+@main
+struct ClimateDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClimateData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClimateData
+//
+//  Created by Bosco Ho on 2024-08-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
